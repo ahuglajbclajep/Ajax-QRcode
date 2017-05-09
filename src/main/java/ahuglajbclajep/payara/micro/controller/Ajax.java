568bfed33f2dc1c96a8063ead40ca474aebe0a47
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-05-09 15:34:14 +0900

    upd create QRcode

--- src/main/java/ahuglajbclajep/payara/micro/controller/Ajax.java
+++ src/main/java/ahuglajbclajep/payara/micro/controller/Ajax.java
@@ -1,20 +1,41 @@
 package ahuglajbclajep.payara.micro.controller;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import javax.imageio.ImageIO;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.EnumMap;
 
-@WebServlet ("/ajax")
+@WebServlet("/ajax")
 public class Ajax extends HttpServlet {
 
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse res) {
-        try {
-            req.getReader().lines().forEach(System.out::println);
-        } catch (IOException e) {
-            e.printStackTrace();
+        res.setContentType("image/jpeg");
+
+        try (OutputStream out = res.getOutputStream()) {
+            ImageIO.write(createQR(req.getReader().readLine()), "jpg", out);
+        } catch (WriterException | IOException e) {
+            res.setStatus(HttpServletResponse.SC_NOT_FOUND);
         }
+    }
+
+    private BufferedImage createQR(String text) throws WriterException {
+        final int size = 171 + 4*2;
+        EnumMap<EncodeHintType, Object> hints = new EnumMap<>(EncodeHintType.class);
+        hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+        BitMatrix bitMatrix = new QRCodeWriter().encode(text, BarcodeFormat.QR_CODE, size, size, hints);
+        return MatrixToImageWriter.toBufferedImage(bitMatrix);
     }
 }

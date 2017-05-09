ed9ca65db9c37e5ca05344edbb35c1479d16e02c
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-05-09 23:38:43 +0900

    change html structure

--- src/main/webapp/js/ajax.js
+++ src/main/webapp/js/ajax.js
@@ -1,16 +1,14 @@
-document.getElementById("form").addEventListener("submit", ajax, false);
+document.getElementById("submit_button").addEventListener("click", ajax, false);
 
 function ajax(event) {
-    event.preventDefault();
-
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("qrcode").src = URL.createObjectURL(this.response);
+            document.getElementById("qrcode_image").src = URL.createObjectURL(this.response);
         }
     };
 
     xhr.open("POST", "ajax");
     xhr.responseType = "blob";
-    xhr.send(document.getElementById("contents").value);
+    xhr.send(document.getElementById("input_text").value);
 }

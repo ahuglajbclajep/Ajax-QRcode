48bb77c4590928d2c8ff88e6c1ab453827e1ff09
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-05-10 19:00:32 +0900

    upd replace to jQuery

--- src/main/webapp/js/ajax.js
+++ src/main/webapp/js/ajax.js
@@ -1,12 +1,12 @@
-document.getElementById("submit_button").addEventListener("click", submit, false);
+$("#submit_button").on("click", submit);
 var hash = location.hash.replace(/^#/, "");
 if (hash !== "") {
     ajax(hash);
-    document.getElementById("input_text").value = hash;
+    $("#input_text").val(hash);
 }
 
 function submit() {
-    var text = document.getElementById("input_text").value;
+    var text = $("#input_text").val();
     ajax(text);
     location.hash = text;
 }
@@ -23,7 +23,7 @@ function ajax(text) {
         }
     })
     .done(function (response) {
-        document.getElementById("qrcode_image").src = URL.createObjectURL(response);
+        $("#qrcode_image").attr("src", URL.createObjectURL(response));
     })
     .fail(function () {
         alert("Connection failed.");

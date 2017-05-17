6a7d7aba0625032116896e9d9ce7c2f459beb4d8
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-05-18 01:07:24 +0900

    upd createQR to class

--- src/main/java/ahuglajbclajep/payara/micro/controller/Servlet.java
+++ src/main/java/ahuglajbclajep/payara/micro/controller/Servlet.java
@@ -1,20 +1,15 @@
 package ahuglajbclajep.payara.micro.controller;
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
+import ahuglajbclajep.payara.micro.model.QRCode;
 import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
-import com.google.zxing.qrcode.QRCodeWriter;
 
 import javax.imageio.ImageIO;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.EnumMap;
 
 @WebServlet("/ajax")
 public class Servlet extends HttpServlet {
@@ -29,25 +24,9 @@ public class Servlet extends HttpServlet {
         res.setContentType("image/jpeg");
 
         try (OutputStream out = res.getOutputStream()) {
-            ImageIO.write(createQR(req.getReader().readLine()), "jpg", out);
+            ImageIO.write(new QRCode().create(req.getReader().readLine()), "jpg", out);
         } catch (WriterException | IOException e) {
             res.setStatus(HttpServletResponse.SC_NOT_FOUND);
         }
-    }
-
-    private BufferedImage createQR(String contents) throws WriterException {
-        // The width and height of the image are determined by first creating an image,
-        // adding a margin (4*2 pixels), and comparing it with the specified length.
-        final int SIZE = 200;
-
-        return MatrixToImageWriter.toBufferedImage(
-                new QRCodeWriter().encode(contents, BarcodeFormat.QR_CODE, SIZE, SIZE,
-                        new EnumMap<EncodeHintType, Object>(EncodeHintType.class) {
-                            {
-                                put(EncodeHintType.CHARACTER_SET, "UTF-8");
-                            }
-                        }
-                )
-        );
     }
 }
